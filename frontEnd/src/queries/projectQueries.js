ae4f1e7b018894abac4523ac6d7895774f0faacc
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-07-05 17:17:57 +0100

    added the gql queries for fetching of all clients

--- frontEnd/src/queries/projectQueries.js
+++ frontEnd/src/queries/projectQueries.js
@@ -2,31 +2,31 @@ import { gql } from '@apollo/client';
 
 // the query below fetches all the projects
 const GET_PROJECTS = gql`
-  query getProjects {
-    projects {
-      id
-      name
-      status
+    query getProjects {
+        projects {
+            id
+            name
+            status
+        }
     }
-  }
 `;
 
 // the query below fetches just one project
 const GET_PROJECT = gql`
-  query getProject($id: ID!) {
-    project(id: $id) {
-      id
-      name
-      description
-      status
-      client {
-        id
-        name
-        email
-        phone
-      }
+    query getProject($id: ID!) {
+        project(id: $id) {
+            id
+            name
+            description
+            status
+            client {
+                id
+                name
+                email
+                phone
+            }
+        }
     }
-  }
 `;
 
 export { GET_PROJECTS, GET_PROJECT };
